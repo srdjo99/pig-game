a1e0d33e6ec9106209399a58b13a7142483cf782
Srdjan <srdjan.coralic@nenasal.com>
2022-03-04 18:46:34 +0100

    Change folder structure

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ btnRoll.addEventListener('click', function () {
     const dice = Math.trunc(Math.random() * 6) + 1;
     // 2. Display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `img/dice-${dice}.png`;
 
     // 3. Check for rolled 1: if true, switch to next player
 
